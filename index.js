832e6015877d48bca2b07e869f234307a14ae93f
Pragadeesh <rpraga50@gmail.com>
2019-06-05 19:48:37 +0530

    js program

---
+++ index.js
@@ -0,0 +1,14 @@
+let name= 'praga';
+if(name!= 'viki')
+{
+    console.log("noo!!");
+}
+else{
+    console.log("yep!!");
+}
+
+function praga(name) {
+
+console.log("hello"+ " "+name);
+} 
+praga("viki");
