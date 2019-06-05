f7cc75a5141a4c71d9c3c5d4716919e5e84c7d44
Pragadeesh <rpraga50@gmail.com>
2019-06-05 20:30:48 +0530

    object,array,function in js

---
+++ object.js
@@ -0,0 +1,17 @@
+//object
+let person = {
+name:"praga",
+age:'17',
+
+}
+console.log(person);
+console.log(person.name);
+console.log(person['age']);
+//array 
+let selectedgame = ['pubg','cod','coc'];
+console.log(selectedgame[2]);
+//function
+function mad(name){
+    console.log('hi'+ " "+name);
+}
+mad('viki don');
